4f7075ce47f77ca00d9cf64e7c02d1768bf3006f
Katie Blalock <katieblalock314@gmail.com>
2020-05-27 12:47:10 -0700

    second commit

--- app.py
+++ app.py
@@ -7,21 +7,32 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         url = request.form["response"]
-        link = str(url.replace('https://www.', ''))
-        path = link.replace('/','%2F')
-        return redirect(url_for("url", u=path))
-    else:
-        return render_template("index.html")
+        validurls = ["godaddy.com", "http://www.godaddy.com", "http://godaddy.com", "http://godaddy.com/",
+                     "https://www.godaddy.com/", "https://www.godaddy.com", "www.godaddy.com", "www.godaddy.com/"]
 
+        if url in validurls:
+            return redirect(url_for("url", u="%2F"))
+
+        for c in validurls:
+            if c in url:
+                link = url.split('.com/')
+                path = link[1]
+                append = path.replace('/', '%2F')
+                return redirect(url_for("url", u=append))
+        else:
+            return f"Please enter a valid link"
+
+    return render_template("index.html")
 
 @app.route("/<u>")
 def url(u):
     u = str(u)
-    r = str('([@T[link:&lt;relative path="')
-    t = str('"/&gt;]@T])')
+    r = str('([@T[link:<relative path="')
+    t = str('"/>]@T])')
     x = r + u + t
-    d = str(x.replace('%2F','/'))
-    return f"<h1>Your token is:</h1>{d}"
+    token = str(x.replace('%2F','/'))
+
+    return render_template("token.html", token=token)
 
 
 if __name__ == "__main__":
---
+++ practice.py
@@ -0,0 +1,4 @@
+url = "https://www.godaddy.com/websites/website-builder"
+link = url.split(".com/")
+path = link[1]
+print(path)
